c2fc91bfe952402c93e700ac2743303f266e47b3
Gochi <mevere66@gmail.com>
2022-02-22 15:43:24 -0500

    Se agrega forms para ingresar los datos del usuario, es posible agregar un usuario y visualizarlo, validaciones en los campos requeridos del usuario implementadas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UserTable from "./components/UserTable";
 import { v4 as uuidv4 } from 'uuid';
+import AddUserForm from "./components/AddUserForm";
 
 function App() {
   const usersData = [
@@ -13,12 +14,21 @@ function App() {
 
   const [users, setUsers] = useState(usersData);
 
+  //Agregar usuarios
+  const addUser= (user) => {
+    user.id=uuidv4();
+    setUsers([
+      ...users,user
+    ])
+  }
+
   return (
     <div className="container">
       <h1>CRUD APP with hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser}/>
         </div>
         <div className="flex-large">
           <h2>View users</h2>
---
+++ src/components/AddUserForm.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { useForm } from "react-hook-form";
+
+const AddUserForm = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data, e) => {
+    
+    //limpiar campos
+    e.target.reset();
+
+    // agregar usuario
+    props.addUser(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        placeholder="Escriba su nombre"
+        {...register("name", {
+          required: true,
+          maxLength: 30,
+        })}
+      />
+      <div>{errors.name?.type === "required" && "Este campo es obligatorio"}</div>
+
+      <label>Username</label>
+      <input
+        type="text"
+        name="username"
+        placeholder="Escriba su username"
+        {...register("username", {
+          required: true,
+          maxLength: 30,
+        })}
+      />
+      <div>
+        {errors.username?.type === "required" && "Este campo es obligatorio"}
+      </div>
+      <button>Add new user</button>
+    </form>
+  );
+};
+
+export default AddUserForm;
