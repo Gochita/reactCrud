d53deebb9805a4fc035aaedf93b519b7a68c546e
Gochi <mevere66@gmail.com>
2022-02-22 16:02:49 -0500

    Se añade estilos a botones delete y edit usando el bootstrap get started. Se añade funcionalidad eliminar, permite eliminar usuarios correctamente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import UserTable from "./components/UserTable";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import AddUserForm from "./components/AddUserForm";
 
 function App() {
   const usersData = [
-    {id: uuidv4(), name: 'Gochi', username: 'tamagochita' },
+    { id: uuidv4(), name: "Gochi", username: "tamagochita" },
     { id: uuidv4(), name: "Jacob", username: "blackwolf" },
     { id: uuidv4(), name: "Rose", username: "rosequartz" },
   ];
@@ -15,12 +15,15 @@ function App() {
   const [users, setUsers] = useState(usersData);
 
   //Agregar usuarios
-  const addUser= (user) => {
-    user.id=uuidv4();
-    setUsers([
-      ...users,user
-    ])
-  }
+  const addUser = (user) => {
+    user.id = uuidv4();
+    setUsers([...users, user]);
+  };
+
+  //Eliminar usuarios
+  const deleteUser = (id) => {
+   setUsers(users.filter(user => user.id !== id))
+  };
 
   return (
     <div className="container">
@@ -28,11 +31,11 @@ function App() {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
-          <AddUserForm addUser={addUser}/>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users}/>
+          <UserTable users={users} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const UserTable = (props) => {
   console.log(props.users);
   return (
@@ -13,24 +12,29 @@ const UserTable = (props) => {
         </tr>
       </thead>
       <tbody>
-        {
-
-            props.users.length > 0 ?
-            props.users.map((user) => (
+        {props.users.length > 0 ? (
+          props.users.map((user) => (
             <tr key={user.id}>
-                <td>{user.name}</td>
-                <td>{user.username}</td>
-                <td>
-                    <button className="button muted-button">Edit</button>
-                    <button className="button muted-button">Delete</button>
-                </td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>
+                <button className="btn btn-secondary">Edit</button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => {
+                    props.deleteUser(user.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-        )) : (
-            <tr>
-                <td colSpan={3}>No users</td>
-            </tr>
-        )
-        }
+          ))
+        ) : (
+          <tr>
+            <td colSpan={3}>No users</td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
