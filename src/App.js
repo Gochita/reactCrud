a86a077f543142b3b48edd900922d28eaaf04bb0
Gochi <mevere66@gmail.com>
2022-02-22 14:43:27 -0500

    Creacion de carpeta components, creacion de componente UserTable con jsx y visualización de la tabla en el app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
-
+import React from "react";
+import UserTable from "./components/UserTable";
 function App() {
   return (
-    <div>hola mundo</div>
+    <div className="container">
+      <h1>CRUD APP with hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable />
+        </div>
+      </div>
+    </div>
   );
 }
 
